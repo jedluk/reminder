c8a1c70daa82043245c9010a2031a7577212e398
jlukasiu <jedrzej.lukasiuk@gmail.com>
2018-05-17 22:43:14 +0200

    Get rid of social links in header (now they are in side bar)

--- public/js/date.js
+++ public/js/date.js
@@ -2,7 +2,7 @@ const m_names = new Array("January", "February", "March",
 "April", "May", "June", "July", "August", "September", 
 "October", "November", "December");
 
-var d = new Date();
+const d = new Date();
 const curr_date = d.getDate();
 let sup = "";
 if (curr_date == 1 || curr_date == 21 || curr_date ==31)
@@ -22,8 +22,7 @@ else
    sup = "th";
    }
 
-var curr_month = d.getMonth();
-var curr_year = d.getFullYear();
+const curr_month = d.getMonth();
+const curr_year = d.getFullYear();
 
-document.getElementById('header-date').innerHTML = 'Today is ' + curr_date + "<SUP>" + sup + "</SUP> " 
-+ m_names[curr_month] + " " + curr_year;
+document.getElementById('header-date').innerHTML = `Today is ${curr_date} <SUP> ${sup}</SUP> ${m_names[curr_month]} ${curr_year}`;
