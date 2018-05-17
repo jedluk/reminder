7df0d12f035842788cf85aec0d2a59e647e4a8e0
jlukasiu <jedrzej.lukasiuk@gmail.com>
2018-05-17 10:32:51 +0200

    Added schemas for notes and users

---
+++ models/Note.js
@@ -0,0 +1,25 @@
+import { MongooseDocument } from 'mongoose';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const NoteSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  }
+});
+
+mongoose.model('notes', Note);
