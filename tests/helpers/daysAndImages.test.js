c5a756f6c0abd416236868c1c0b5468358f0d4a7
jlukasiu <jedrzej.lukasiuk@gmail.com>
2018-06-02 13:46:16 +0200

    Added basic test structure

---
+++ tests/helpers/daysAndImages.test.js
@@ -0,0 +1,9 @@
+const mocha = require('mocha');
+const assert = require('chai').assert;
+const { getImgNames, randomNoRepeats, prepareDayStructure } = require('../../helpers/daysAndImages');
+
+describe('day and images helpers methods', () => {
+  it('should be true', () => {
+    assert.equal(1,1);
+  })
+})
---
+++ tests/helpers/hbs.test.js
@@ -0,0 +1,10 @@
+const mocha = require('mocha');
+const assert = require('chai').assert;
+const { isFriday } = require('../../helpers/hbs');
+
+describe('handlebars helper methods', () => {
+  it('should be true', () => {
+    const id = 1;
+    assert.equal(1,1);
+  });
+})
